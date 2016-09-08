affc3f8979847cfee3360e9c501965cebeb4aa1c
Yves Gross <why-y@gmx.net>
2016-09-08 21:40:23 +0200

    prepare for sets

--- src/main/java/ch/gry/scorer/Set.java
+++ src/main/java/ch/gry/scorer/Set.java
@@ -1,14 +1,52 @@
 package ch.gry.scorer;
-import static ch.gry.scorer.Set.Mode.*;
+import static ch.gry.scorer.Set.Mode.WITHOUT_TIEBREAK;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Set {
 	enum Mode{WITH_TIEBREAK, WITHOUT_TIEBREAK}
 	private Mode mode =  WITHOUT_TIEBREAK;
+	private Player[] players = new Player[2];
+	private List<Player> gameSequence;
 	
 	private Set() {
+		gameSequence = new ArrayList<>();
 	}
 	
-	public static Set create(String firstServerName, String firstReturnerName, Mode setMode) {
-		return null;
+	public static Set create(Player firstServer, Player firstReturner, Mode setMode) {
+		Set set = new Set();
+		set.setServer(firstServer);
+		set.setReturner(firstReturner);
+		set.mode = setMode;
+		return set;
+	}
+
+	private void setServer(Player player) { 
+		players[0] = player; 
+	}
+	
+	private Player getServer() { 
+		return players[0]; 
+	}
+	
+	private void setReturner(Player player) { 
+		players[1] = player; 
+	}
+	
+	private Player getReturner() {
+		return players[1];
+	}
+
+	private long gamesOf(Player player) {
+		return gameSequence.stream().filter(p -> p==player).count();
+	}
+
+	public String getScore() {
+		return String.format("%d:%d", gamesOf(getServer()), gamesOf(getReturner())); 
+	}
+
+	public void gameFor(Player player) {
+		gameSequence.add(player);
 	}
 }
--- src/test/java/ch/gry/scorer/SetTest.java
+++ src/test/java/ch/gry/scorer/SetTest.java
@@ -1,23 +1,44 @@
 package ch.gry.scorer;
 
+import static ch.gry.scorer.Set.Mode.WITHOUT_TIEBREAK;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-import static ch.gry.scorer.Set.Mode.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 
 
 public class SetTest {
 	
-	Set testSet;
-	
+	private Set testSet;
+	private Player firstServer;
+	private Player firstReturner;
+
 	@Before
 	public void setUp() {
+		firstServer =  Player.create("Tom");
+		firstReturner = Player.create("Pat");
+		testSet = Set.create(firstServer, firstReturner, WITHOUT_TIEBREAK);
 	}
 
 	@Test
 	public void createSet() throws Exception {
-		testSet = Set.create("A", "B", WITHOUT_TIEBREAK);
-		
+		assertThat(firstServer, is(not(nullValue())));
+		assertThat(firstReturner, is(not(nullValue())));
+		assertThat(testSet, is(not(nullValue())));
+	}
+	
+	@Test
+	public void get_initial_score() throws Exception {
+		assertThat(testSet.getScore(), is(equalTo("0:0")));
+	}
+	
+	@Test
+	public void test_1_0() throws Exception {
+		testSet.gameFor(firstServer);
+		assertThat(testSet.getScore(), is(equalTo("1:0")));
 	}
 }
